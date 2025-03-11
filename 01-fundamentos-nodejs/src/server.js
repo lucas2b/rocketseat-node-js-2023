354c12951ed832e63a84f1be533ee3a688e14022
Lucas Barros Bonine <lucasbonine@live.com>
2025-03-11 08:27:49 -0300

    Update server.js

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,28 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response)=> {
-    return response.end('Hello World!');
+
+    const {method, url} = request;
+
+    if(method === 'GET' && url === '/users'){
+        return response
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users));
+    }
+
+    if(method === 'POST' && url == '/users'){
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        });
+
+        return response.writeHead(201).end();
+    }
+
+    return response.writeHead(404).end();
 });
 
 server.listen(3333);
